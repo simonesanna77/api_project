5d315f85f09a70a25852a6fc1c83160dd5b67f35
Simone Sanna <sannasimo@gmail.com>
2020-09-17 17:57:14 +0200

    changes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,19 +1,81 @@
+const BootcampModel = require('../models/Bootcamp');
+
 exports.getBootcamps = (req, res, next)=> {
-  res.status(200).json({success: true, msg: 'show all bootcamps', hello: req.hello});
+  BootcampModel.find().then(data => {
+    res.status(200).json({
+      success: true,
+      data: data
+    });
+  }).catch((error) => {
+    console.log(error);
+    res.status(400).json({success: false, error: error});
+  });
 };
 
 exports.getBootcamp = (req, res, next)=> {
-  res.status(200).json({success: true, msg: `get bootcamp ${req.params.id}`});
+  BootcampModel.findById(req.params.id).then(data => {
+
+    if(!data) {
+      return res.status(400).json({success: false, error: 'item not found'});
+    }
+
+    res.status(200).json({
+      success: true,
+      data: data
+    });
+  }).catch((error) => {
+    console.log(error);
+    res.status(400).json({success: false, error: error});
+  });
+  //res.status(200).json({success: true, msg: `get bootcamp ${req.params.id}`});
 };
 
 exports.createBootcamp = (req, res, next)=> {
-  res.status(200).json({success: true, msg: 'create bootcamps'});
+  BootcampModel.create(req.body).then(data => {
+    res.status(201).json({
+      success: true,
+      data: data
+    });
+  }).catch((error) => {
+    console.log(error);
+    res.status(400).json({success: false, error: error});
+  });
 };
 
 exports.updateBootcamp = (req, res, next)=> {
-  res.status(200).json({success: true, msg: `update bootcamp ${req.params.id}`});
+  BootcampModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  }).then(data => {
+
+    if(!data) {
+      return res.status(400).json({success: false, error: 'item not found'});
+    }
+
+    res.status(200).json({
+      success: true,
+      data: data
+    });
+  }).catch((error) => {
+    console.log(error);
+    res.status(400).json({success: false, error: error});
+  });
+  //res.status(200).json({success: true, msg: `update bootcamp ${req.params.id}`});
 };
 
 exports.deleteBootcamp = (req, res, next)=> {
-  res.status(200).json({success: true, msg: `delete bootcamp ${req.params.id}`});
+  BootcampModel.findByIdAndDelete(req.params.id).then(data => {
+
+    if(!data) {
+      return res.status(400).json({success: false, error: 'item not found'});
+    }
+
+    res.status(200).json({
+      success: true,
+      data: data
+    });
+  }).catch((error) => {
+    console.log(error);
+    res.status(400).json({success: false, error: error});
+  });
 };
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const routes = require('./routes/bootcamp');
 
 const app = express();
 
+//body parser
+app.use(express.json());
+
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
